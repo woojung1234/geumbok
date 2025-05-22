106c3d125473c1d496dc0a44dce36a4133ff2498
woojung1234 <84664664+woojung1234@users.noreply.github.com>
2025-05-22 10:05:50 +0900

    Fix app/_layout.js to use react-native useColorScheme hook

--- app/_layout.js
+++ app/_layout.js
@@ -4,12 +4,12 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { RecoilRoot } from 'recoil';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useColorScheme } from 'react-native';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '../hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
