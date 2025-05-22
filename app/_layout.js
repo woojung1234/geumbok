1bdb9d2f600c3368b808208654913217268e1853
woojung1234 <84664664+woojung1234@users.noreply.github.com>
2025-05-22 09:50:13 +0900

    Convert _layout.tsx to JavaScript

---
+++ app/_layout.js
@@ -0,0 +1,34 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { RecoilRoot } from 'recoil';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '../hooks/useColorScheme';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <RecoilRoot>
+      <SafeAreaProvider>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </RecoilRoot>
+  );
+}
